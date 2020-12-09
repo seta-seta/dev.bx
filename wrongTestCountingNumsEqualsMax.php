082c77c0ab1d797353cb73915cb01050755cd996
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-09 12:14:59 +0200

    wrongTestCountingNumsEqualsMax.php: тест для неправильных данных функции countingNumsEqualsMax

--- wrongTestCountingNumsEqualsMax.php
+++ wrongTestCountingNumsEqualsMax.php
@@ -4,15 +4,15 @@ require_once('readFromConsole.php');
 require_once('countingNumsEqualsMax.php');
 require_once('assertEquals.php');
 
-function correctTestCountingNumsEqualsMax()
+function wrongTestCountingNumsEqualsMax()
 {
   $testNumsArray = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 !stop';
 	echo assertEquals(0, countingNumsEqualsMax(readFromConsole('',$testNumsArray)), 'Передано больше чем 20 чисел').PHP_EOL.PHP_EOL;
 
-	$testNumsArray = '';
+  $testNumsArray = '';
 	echo assertEquals(0, countingNumsEqualsMax(readFromConsole('',$testNumsArray)), 'Последовательность не передана').PHP_EOL;
   
-	$testNumsArray = '!stop';
+  $testNumsArray = '!stop';
 	echo assertEquals(0, countingNumsEqualsMax(readFromConsole('',$testArray)), 'Передано только ключевое слово !stop').PHP_EOL;
   
   $testNumsArray = '1 2 3 false !stop';
@@ -28,4 +28,4 @@ function correctTestCountingNumsEqualsMax()
 	echo assertEquals(0, countingNumsEqualsMax(readFromConsole('',$testNumsArray)), 'Передано отрицательное число').PHP_EOL;
 }
 
-correctTestCountingNumsEqualsMax();
+wrongTestCountingNumsEqualsMax();
