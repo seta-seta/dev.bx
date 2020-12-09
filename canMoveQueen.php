ad9e787a595bb17e286ad96215783bdf220075c8
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-09 14:37:02 +0200

    canMoveQueen.php : функция для проверки движения королевы

---
+++ canMoveQueen.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('classQueen.php');
+require_once('readFromConsole.php');
+
+function canMoveQueen(array ($coordinates = null))
+{
+	// Ввод координат с консоли
+	if ($coordinates == null)
+	{
+		$coordinates = readFromConsole("Введите координаты текущего положения королевы (номер столбца, номер строки) и положения на которое хотите передвинуть фигуру". PHP.EOL);
+		$coordinates = explode(' ', $coordinates);
+		$coordinates = array_map(function($num){return (int)$num;}, $coordinates); // функция возвращает массив с координатами
+	}
+
+	// проверка на правильность ввода координат
+	if (count($coordinates) !== 4)
+	{
+		return ('Введено не верное количество координат, повторите ввод'. PHP_EOL);
+	}
+  
+  // проверка введенных координат 
+	foreach ($coordinates as $num)
+	{
+		if($num <=0 || $num > 8)
+		{
+			return ('Не верно введены координаты, повторите ввод'. PHP_EOL);
+		}
+	}
+
+	// передвижение фигуры при помощи класса Королева
+	$queen = new Queen($coordinates[0], $coordinates[1], '');
+	return $queen->canMove($coordinates[2], $coordinates[3]);
+}
