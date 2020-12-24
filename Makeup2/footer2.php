5410c63ecde217fc1aa02b4de10253e7cb681a34
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 16:04:29 +0200

    footer2.php - подвал страницы №2

---
+++ Makeup2/footer2.php
@@ -0,0 +1,39 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-4 text-white text-left">
+				<div class="row"> <div class="logo">TRADE</div></div>
+				<div class="row">© 2010 — 2020</div>
+				<div class="row ">Privacy — Terms</div>
+			</div>
+			<div class="col-2 text-white text-left">
+				<div class="row font-weight-bold">Product</div>
+				<div class="row">Product</div>
+				<div class="row">Product</div>
+				<div class="row">Product</div>
+			</div>
+			<div class="col-2 text-white text-left">
+				<div class="row font-weight-bold">Features</div>
+				<div class="row">Features</div>
+				<div class="row">Features</div>
+				<div class="row">Features</div>
+			</div>
+			<div class="col-2 text-white text-left">
+				<div class="row font-weight-bold">Resources</div>
+				<div class="row">Resources</div>
+				<div class="row">Resources</div>
+				<div class="row">Resources</div>
+			</div>
+			<div class="col-2 text-white text-left">
+				<div class="row font-weight-bold">Company</div>
+				<div class="row">Company</div>
+				<div class="row">Company</div>
+				<div class="row">Company</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+</div>
+</body>
+</html>
