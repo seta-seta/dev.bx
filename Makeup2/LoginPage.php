0cb65a4088aef3213049aa8c78d18f3b4da1e866
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 16:00:03 +0200

    LoginPage.php - страница LoginPage

---
+++ Makeup2/LoginPage.php
@@ -0,0 +1,31 @@
+<?php include_once "header.php";?>
+
+
+<div class="LoginPage">
+	<div class="content-form text-center">
+		<h4>Try the product out for free.</h4>
+		<form>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">
+					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="email"/>
+				</label>
+			</div>
+		</form>
+		<form>
+			<div class="form-group">
+				<label for="exampleFormControlInput1">
+					<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="password"/>
+				</label>
+			</div>
+		</form>
+		<div class="col">
+			<a href="" class="btn btn-lg btn-dark">Start free trial</a>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<?php include_once "footer2.php";
