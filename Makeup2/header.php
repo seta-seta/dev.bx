5dfd607c2e64fcdd5aa1804019270f780fc489cd
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 16:05:14 +0200

    header.php - заголовок страницы

---
+++ Makeup2/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+	<title>HW2</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+		  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"/>
+	<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<div class="wrap">
+	<header class="container-fluid">
+		<div class="row">
+			<div class="col">
+				<div class="nav">
+					<a href="" class="nav-link text-white">Link 1</a>
+					<a href="" class="nav-link text-white">Link 2</a>
+					<a href="" class="nav-link text-white">Link 3</a>
+					<a href="" class="nav-link text-white">Link 4</a>
+				</div>
+			</div>
+			<div class="col">
+				<div class="logo text-center">TRADE</div>
+			</div>
+			<div class="col text-right btn-top">
+				<a href="" class="btn btn-lg btn-warning">Free trial</a>
+			</div>
+		</div>
+	</header>
