402cf89b861b6ed8bb2d81ed338b557dbfd0305f
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 15:56:43 +0200

    HomePage.php - страница HomePage

---
+++ Makeup2/HomePage.php
@@ -0,0 +1,88 @@
+<?php include_once "header.php";?>
+
+<div class="banner d-flex align-items-center justify-content-center">
+	<div class="banner-overlay"></div>
+	<div class="banner-content">
+		<h1 class="text-white text-center banner-heading">Value Proposition</h1>
+		<p class="text-white text-center banner-paragraph">Lorem ipsum dolor sit amet,
+			consectetur adipiscing elit,
+			sed do eiusmod tempor incididunt
+			ut labore et dolore magna aliqua.
+		</p>
+		<div class="col text-center btn-middle">
+			<a href="" class="btn btn-lg btn-warning btn-banner">Start free trial</a>
+		</div>
+	</div>
+</div>
+
+<div class="top-3-moto container">
+	<h3 class="top3moto-heading text-center">Your Best Value Proposition</h3>
+	<p class="text-center top3moto-paragraph">Lorem ipsum dolor sit amet,
+		consectetur adipiscing elit,
+		sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua.
+		Ut enim ad minim veniam,
+	</p>
+	<div class="row top3moto-images">
+		<div class="col">
+			<div class="card">
+				<div class="alignImage">
+					<img src="images/moto1.png" class="card-img-top" alt="...">
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+					<a href="">Learn more</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card">
+				<div class="alignImage">
+					<img src="images/moto2.png" class="card-img-top" alt="...">
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+					<a href="">Learn more</a>
+				</div>
+			</div>
+		</div>
+		<div class="col">
+			<div class="card">
+				<div class="alignImage">
+					<img src="images/moto3.png" class="card-img-top" alt="...">
+				</div>
+				<div class="card-body">
+					<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+					<a href="">Learn more</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="feature container">
+	<div class="row feature-content">
+		<div class="col">
+			<h4>Feature that is amazing</h4>
+			<p>Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt
+				ut labore et dolore magna aliqua.
+				Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt ut labore
+				et dolore magna aliqua. Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua.
+			</p>
+		</div>
+		<div class="col">
+			<img src="images/moto4.png" class="card-img-top" alt="...">
+		</div>
+	</div>
+</div>
+
+<?php
+include_once "footer1.php";
+include_once "footer2.php";
