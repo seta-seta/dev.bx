5856c6c4704eef5e9559567005829cb634d8380d
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 16:02:12 +0200

    DetaliPage.php - страница DetaliPage

---
+++ Makeup2/DetaliPage.php
@@ -0,0 +1,88 @@
+<?php include_once "header.php";?>
+
+<div class="content-details">
+	<div class="container">
+		<div class="row">
+			<div class="col-7">
+				<h1 class="font-weight-bold">Feature that is amazing</h1>
+				<p>Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit,
+					sed do eiusmod tempor incididunt ut
+					labore et dolore magna aliqua. Lorem
+					ipsum dolor sit amet, consectetur adipiscing
+					elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua.
+				</p>
+				<div class="col-4">
+					<ul class="content-details-ul">
+						<li>
+							Benefit of Feature
+						</li>
+						<li>
+							Benefit of Feature
+						</li>
+						<li>
+							Benefit of Feature
+						</li>
+					</ul>
+				</div>
+				<div class="col text-left btn-middle-detailPage">
+					<a href="" class="btn btn-lg btn-warning">Buy</a>
+				</div>
+			</div>
+			<div class="col-5">
+				<img src="images/moto4.png" alt=""/>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="content-table">
+	<div class="container">
+		<div class="row"><h1 class="font-weight-bold">Value Proposition</h1></div>
+		<div class="row">
+			<table class="table table-bordered">
+				<thead>
+					<tr class="table-secondary">
+						<th scope="col" class="text-center smallCol">Value Name</th>
+						<th scope="col" class="text-left">Property</th>
+					</tr>
+				<tbody>
+				<tr>
+					<td>First value proposition</td>
+					<td>First value proposition</td>
+				</tr>
+				<tr>
+					<td>Second value proposition</td>
+					<td>Second value proposition</td>
+				</tr>
+				</tbody>
+				<thead>
+				<tr>
+					<td colspan="2" class="text-center font-weight-bold table-secondary">Addinational Property</td>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>First value proposition</td>
+					<td>First value proposition</td>
+				</tr>
+				<tr>
+					<td>Second value proposition</td>
+					<td>Second value proposition</td>
+				</tr>
+				</tbody>
+
+			</table>
+		</div>
+		<div class="row">
+			<div class="col text-left btn-middle-detailPage">
+				<a href="" class="btn btn-lg btn-warning">Buy</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+include_once "footer1.php";
+include_once "footer2.php";
