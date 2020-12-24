533f9a9bbf96eb42505cbd51df9faa94fde4ccde
Святослав Гладкий <73549784+seta-seta@users.noreply.github.com>
2020-12-24 16:01:00 +0200

    ProductListPage.php - страница ProductListPage

---
+++ Makeup2/ProductListPage.php
@@ -0,0 +1,122 @@
+<?php include_once "header.php";?>
+
+<div class="best-moto">
+	<h3 class="top3moto-heading text-center">Your Best Value Proposition</h3>
+	<p class="text-center top3moto-paragraph">Lorem ipsum dolor sit amet,
+		consectetur adipiscing elit,
+		sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua.
+		Ut enim ad minim veniam,
+	</p>
+	<div class="content-best-moto container">
+		<div class="row top3moto-images">
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto1.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto2.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto3.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row top3moto-images">
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto1.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto2.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto3.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row top3moto-images">
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto1.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto2.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<div class="alignImage">
+						<img src="images/moto3.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h5>
+						<a href="">Learn more</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+include_once "footer1.php";
+include_once "footer2.php";
